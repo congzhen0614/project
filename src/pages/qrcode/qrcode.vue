2d5453a76f9525fe819cf4664e50f3bfe4c9c17c
xbbSu1y <876133937@qq.com>
2018-06-13 16:40:49 +0800

    20180613

--- src/pages/qrcode/qrcode.vue
+++ src/pages/qrcode/qrcode.vue
@@ -2,7 +2,11 @@
   <div class="qrcode">
     <v-head :title="title"></v-head>
     <section>
-      <div id="qrcode" ref="qrcode"></div>
+      <ul>
+        <li v-for="(item, index) in codeList" :key="index">
+          <div id="qrcode" class="qrcode-list" ref="qrcode"></div>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -16,15 +20,22 @@ export default {
   },
   data () {
     return {
-      title: '生成二维码'
+      title: '生成二维码',
+      codeList: ['2018061301', '2018061302', '2018061303', '2018061304', '2018061305']
     }
   },
   mounted () {
-    this.qrcode()
+    this.setCodeList()
   },
   methods: {
-    qrcode () {
-      let qrcode = new QRCode(this.$refs.qrcode, {
+    setCodeList () {
+      this.codeList.forEach((item, index) => {
+        let qrcodeList = document.getElementsByClassName('qrcode-list')[index]
+        this.qrcode(qrcodeList)
+      })
+    },
+    qrcode (domName) {
+      let qrcode = new QRCode(domName, {
         text: '201806131534',
         width: 100,
         height: 100,
