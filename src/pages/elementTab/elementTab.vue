00279213ba08026e83f1b4f1ae475d74ce3b806d
xbbSu1y <876133937@qq.com>
2018-12-18 18:49:21 +0800

    20181218

--- src/pages/elementTab/elementTab.vue
+++ src/pages/elementTab/elementTab.vue
@@ -1,24 +1,16 @@
 <template>
-  <div class="element-tab">
+  <div class="element-tab" @click="onClose">
     <nav class="tab-nav">
       <ul>
-        <li class="navAct" @contextmenu.prevent="onMousedown">
-          <span>首页</span>
-          <i class="el-icon-close"></i>
-        </li>
-        <li @contextmenu.prevent="onMousedown">
-          <span>杂志管理</span>
-          <i class="el-icon-close"></i>
-        </li>
-        <li @contextmenu.prevent="onMousedown">
-          <span>年龄管理</span>
-          <i class="el-icon-close"></i>
-        </li>
-        <li @contextmenu.prevent="onMousedown">
-          <span>分类管理</span>
+        <li :class="{navAct: item === '首页'}" v-for="(item, index) in list" :key="index" @contextmenu.prevent="onMousedown($event, index)">
+          <span>{{ item }}</span>
           <i class="el-icon-close"></i>
         </li>
       </ul>
+      <div ref="navClose" class="tab-nav-close">
+        <p @click="onCloseOther">关闭其他</p>
+        <p @click="onCloseAll">关闭全部</p>
+      </div>
     </nav>
   </div>
 </template>
@@ -27,17 +19,42 @@
 export default {
   name: 'element-tab',
   data () {
-    return {}
+    return {
+      list: ['首页', '杂志管理', '年龄管理', '分类管理', '目录表单', '条码管理', '订单管理'],
+      closeIndex: 0
+    }
   },
   methods: {
-    onMousedown () {
-      console.log('点击右键')
+    onClose () {
+      this.$refs.navClose.style.display = 'none'
+    },
+    onMousedown (event, index) {
+      this.$refs.navClose.style.left = (event.pageX - event.offsetX) + 'px'
+      this.$refs.navClose.style.display = 'block'
+      this.closeIndex = index
+    },
+    onCloseOther () {
+      let list = []
+      list.push(this.list[0])
+      list.push(this.list[this.closeIndex])
+      this.list = list
+    },
+    onCloseAll () {
+      let list = []
+      list.push(this.list[0])
+      this.list = list
     }
   }
 }
 </script>
 
 <style>
+  .element-tab {
+    height: 100%;
+  }
+  .tab-nav {
+    position: relative;
+  }
   .tab-nav ul {
     display: flex;
     flex-wrap: wrap;
@@ -62,5 +79,19 @@ export default {
   }
   .tab-nav li:hover {
     color: #409eff;
+  }
+  .tab-nav-close {
+    position: absolute;
+    top: 28px;
+    width: 70px;
+    display: none;
+    font-size: 12px;
+    text-align: center;
+    background-color: #cccccc;
+  }
+  .tab-nav-close p {
+    cursor: pointer;
+    height: 36px;
+    line-height: 36px;
   }
 </style>
