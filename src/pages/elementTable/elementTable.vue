36eec0795b2755eef50e806a44699925f409b051
xbbSu1y <876133937@qq.com>
2018-12-19 18:21:55 +0800

    20181219

--- src/pages/elementTable/elementTable.vue
+++ src/pages/elementTable/elementTable.vue
@@ -1,106 +1,62 @@
 <template>
   <div class="element-table">
-    <el-button style="margin-bottom: 10px" size="mini" type="primary" @click="loadData">检索</el-button>
-    <el-table ref="multipleTable" :data="tableData" border @select="handleSelect" @select-all="handleSelect">
-      <el-table-column type="selection" width="45" align="center"></el-table-column>
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table :data="tableData6" :span-method="objectSpanMethod" border style="width: 100%; margin-top: 20px">
+      <el-table-column prop="id" label="ID" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="amount1" label="数值 1（元）"></el-table-column>
+      <el-table-column prop="amount2" label="数值 2（元）"></el-table-column>
+      <el-table-column prop="amount3" label="数值 3（元）"></el-table-column>
     </el-table>
-    <el-pagination
-      @current-change="handleCurrentChange"
-      :current-page="pages.pageNum"
-      :page-size="pages.pageSize"
-      :total="pages.totle"
-      layout="total, prev, pager, next">
-    </el-pagination>
-    <el-button size="mini" type="primary" @click="onSubmit">保存提交</el-button>
   </div>
 </template>
 
 <script>
-import tableData from '@/store/tableList/tableList.json'
 export default {
   name: 'element-table',
   data () {
     return {
-      tableData: [],
-      selectData: [],
-      historyData: [],
-      form: {},
-      pages: {
-        pageNum: 1,
-        pageSize: 10,
-        totle: 0
-      }
-    }
-  },
-  created () {
-    this.loadData()
-  },
-  methods: {
-    loadData () {
-      this.tableData = []
-      this.pages.totle = tableData.data.length
-      let pageStart = (this.pages.pageNum - 1) * this.pages.pageSize
-      for (let i = pageStart; i < pageStart + this.pages.pageSize; i++) {
-        this.tableData.push(tableData.data[i])
-      }
-      this.$nextTick(() => {
-        if (typeof this.historyData[this.pages.pageNum] !== 'undefined') {
-          for (let i = 0; i < this.tableData.length; i++) {
-            this.historyData[this.pages.pageNum].forEach(item => {
-              if (item.id === this.tableData[i].id) {
-                this.$refs.multipleTable.toggleRowSelection(this.tableData[i], true)
-              }
-            })
-          }
+      tableData6: [
+        {
+          id: '12987122',
+          name: '王小虎',
+          amount1: '234',
+          amount2: '3.2',
+          amount3: 10
+        },
+        {
+          id: '12987122',
+          name: '王小虎',
+          amount1: '165',
+          amount2: '4.43',
+          amount3: 12
+        },
+        {
+          id: '12987122',
+          name: '王小虎',
+          amount1: '324',
+          amount2: '1.9',
+          amount3: 9
+        },
+        {
+          id: '12987125',
+          name: '王小虎',
+          amount1: '621',
+          amount2: '2.2',
+          amount3: 17
+        },
+        {
+          id: '12987126',
+          name: '王小虎',
+          amount1: '539',
+          amount2: '4.1',
+          amount3: 15
         }
-      })
-    },
-    handleSelect (val) {
-      this.selectData = []
-      this.historyData[this.pages.pageNum] = val
-      this.historyData.forEach(item => {
-        this.selectData = this.selectData.concat(item)
-      })
-    },
-    handleCurrentChange (val) {
-      this.pages.pageNum = val
-      this.loadData()
-    },
-    onSubmit () {
-      console.log(this.selectData)
+      ]
     }
   },
-  watch: {
-    selectData (val) {
-      console.log(val)
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style>
-  .element-table {
-    width: 700px;
-    margin: 0 auto;
-    margin-top: 50px;
-  }
-  .element-table .el-table .cell {
-    font-size: 14px;
-  }
-  .element-table .el-table td, .element-table .el-table th {
-    padding: 5px 0;
-  }
-  .element-table .el-input {
-    font-size: 12px;
-  }
-  .element-table .el-input__inner {
-    height: 30px;
-    line-height: 30px;
-  }
-  .element-table .el-form-item__label {
-    font-size: 12px;
-  }
 </style>
